1f962e1b375b1b43ab75c0a2de2e347a76b4da6c
mzsif <markus.zsifkovits>
2016-11-07 12:33:16 +0100

    final version

--- src/fh/java/geometry/Main.java
+++ src/fh/java/geometry/Main.java
@@ -10,17 +10,74 @@ import java.util.*;
 import java.util.stream.Collectors;
 
 public class Main {
-		//TODO: main() is missing
+
+
+    public static void main(String[] args) {
+
+        Main main = new Main();
+
+        main.doIt();
+
+    }
+
+    private void doIt() {
+
+        List<GeometricShape> shapeList = new ArrayList<>();
+
+        fillList(shapeList);
+
+        printList(shapeList);
+
+        List<Circle> circleList = new ArrayList<>();
+
+        for (GeometricShape shape : shapeList) {
+            if (shape instanceof Circle) {
+                circleList.add((Circle)shape);
+            }
+        }
+        System.out.println("---------------------");
+        for (Circle circle : circleList) {
+            System.out.println(circle);
+        }
+    }
 
 
     public void fillList(List<GeometricShape> shapeList) {
-		//TODO: Implement
+
+        shapeList.add(new Circle(1,2,3.3));
+        shapeList.add(new Circle(1,4,3.1));
+        shapeList.add(new Circle(4,2,3.2));
+        shapeList.add(new Circle(1,3,3.5));
+
+        shapeList.add(new Rectangle(1,2,3.0,3.2));
+        shapeList.add(new Rectangle(1,2,3.0,5.5));
+
+        shapeList.add(new RightTriangle(1,2,3.0,2.2));
+        shapeList.add(new RightTriangle(3,3,2.2,4.2));
 
     }
 
 
     public void printList(List<GeometricShape> shapeList) {
-		//TODO: Implement
+
+
+        System.out.println("------------ for ----------");
+        for (int pos = 0; pos < shapeList.size(); pos++) {
+            System.out.println(shapeList.get(pos));
+        }
+
+        System.out.println("-------- for each --------");
+        for (GeometricShape shape : shapeList) {
+            System.out.println(shape);
+
+        }
+        System.out.println("------------ stream 1 ----------");
+        shapeList.stream()
+                .forEach(shape -> System.out.println(shape));
+
+        System.out.println("------------ for ----------");
+        shapeList.stream().forEach(System.out::println);
+
 
     }
 
